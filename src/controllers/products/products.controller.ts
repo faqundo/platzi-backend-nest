56f3b15286a168f7e4e5a69b4c8f9562efe0dd40
faqundo <facundo.marcial.ferro@gmail.com>
2022-04-05 15:02:04 -0300

    refactor and scalable controller and service

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,8 +1,11 @@
 import { Controller, Get, Post, Param, HttpStatus, HttpCode, Res, Body, Put, Delete } from '@nestjs/common';
 import { Response } from 'express';
+import { ProductsService } from 'src/services/products/products.service';
 
 @Controller('products')
 export class ProductsController {
+
+    constructor( private productsService: ProductsService)
 
     @Get('products/:id')
     getProduct(@Param() params: any): string {
@@ -15,30 +18,29 @@ export class ProductsController {
         @Res() response: Response,
         @Param('productId') productId: string
     ) {
-        response.status(200).send({
+        /* response.status(200).send({
             message: 'acción de encontrar un producto por id',
-        }); // 👈 Using express directly
+        }); // 👈 Using express directly */
+        return this.productsService.findOne(+productId)
     }
 
     @Post() // 👈 New decorator
     create(@Body() payload: any) {
-        return {
+        /* return {
             message: 'accion de crear',
             payload,
-        };
+        }; */
+        return this.productsService.create(payload);
     }
 
     @Put()
     update(@Param('id') id: number, @Body() payload: any) {
-        return {
-            id,
-            payload
-        }
+        return this.productsService.update(id,payload);
     }
 
     @Delete(':id')
-    delete(@Param('id') id: number) {
-        return id;
-    }
+  delete(@Param('id') id: number) {
+    return id;
+  }
 
 }
