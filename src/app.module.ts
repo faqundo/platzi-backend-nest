56194091652de27184ff7c24fde0021ca3a0db2e
faqundo <facundo.marcial.ferro@gmail.com>
2022-04-05 14:42:55 -0300

    structure files and create entity and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsController } from './controllers/products/products.controller';
+import { ProductsService } from './services/products/products.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ProductsController],
+  providers: [AppService, ProductsService],
 })
 export class AppModule {}
