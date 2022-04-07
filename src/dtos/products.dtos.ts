339c663fc938ae3d86a377d238d9a3a28a710af0
faqundo <facundo.marcial.ferro@gmail.com>
2022-04-07 00:43:35 -0300

    class validation in dtos

--- src/dtos/products.dtos.ts
+++ src/dtos/products.dtos.ts
@@ -1,15 +1,30 @@
+import { IsString, IsNumber , IsUrl} from "class-validator";
+
 export class CreateProductDto {
-    readonly name: string;
-    readonly description: string;
-    readonly price: number;
-    readonly stock: number;
-    readonly image: string;
-  }
-  
-  export class UpdateProductDto {
-    readonly name?: string;
-    readonly description?: string;
-    readonly price?: number;
-    readonly stock?: number;
-    readonly image?: string;
-  }
+  @IsString()
+  readonly name: string;
+
+  readonly description: string;
+
+  @IsNumber()
+  readonly price: number;
+
+  readonly stock: number;
+  readonly image: string;
+}
+
+export class UpdateProductDto {
+  @IsString()
+  readonly name?: string;
+
+  readonly description?: string;
+
+  @IsNumber()
+  readonly price?: number;
+
+  @IsNumber()
+  readonly stock?: number;
+
+  @IsUrl()
+  readonly image?: string;
+}
